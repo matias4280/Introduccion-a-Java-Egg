bc8d578ac57ba0f4f49c1cfca1016ebab074c48c
matias4280 <44714657+matias4280@users.noreply.github.com>
2023-04-14 04:22:36 -0300

    guia3_extra, se agrega ej12_v2

--- Guia3-extras-Java/src/guia3/extras/java/ej12_extra_v2.java
+++ Guia3-extras-Java/src/guia3/extras/java/ej12_extra_v2.java
@@ -14,8 +14,42 @@ public class ej12_extra_v2 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+   public static void main(String[] args) {
+
+			String unidad;
+			String decena;
+			String centena;
+		
+		for(int i = 0; i<10; i++) {
+			
+			for (int j = 0; j<10; j++) {
+				
+				for (int k = 0; k<10; k++) {
+					if(i == 3) {
+						centena = "E";
+					}
+					else {
+						centena = String.valueOf(i);
+					}
+					if(j == 3) {
+						decena = "E";
+					}
+					else {
+						 decena = String.valueOf(j);
+					}
+					if(k == 3) {
+						unidad = "E";
+					}
+					else {
+						unidad = String.valueOf(k);
+					}
+					
+					System.out.println(centena+"-" + decena+"-"+ unidad);
+				}
+				
+			}
+		}
+
+	}
+
 }
